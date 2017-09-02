d0866d5c1b607556e0b76d2cf1e4f10f0967e2ce
sans <sergioans95@hotmail.com>
2017-09-02 12:12:49 -0300

    me comprometo

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, y = 0;
+            Console.WriteLine("Ingrese un numero");
+            x = int.Parse(Console.ReadLine());
+            for(int a =1; a <= x; a++)
+            {
+                if (x % a == 0)
+                    y++;
+                
+            }
+            if (y > 2)
+            {
+                Console.WriteLine("Falso ");
+            }else
+            {
+                Console.WriteLine("verdadero ");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
