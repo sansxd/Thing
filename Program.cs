cfa24fbef6376c2f618921484ed32e199000c85a
sansxd <sergioans95@hotmail.com>
2017-10-20 08:51:22 -0300

    Add files via upload
    algoritmo de bubble sort

--- Program.cs
+++ Program.cs
@@ -4,30 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace consola
+namespace DataStructureAlgoriths
 {
+    class ArrayBubble
+    {
+        public void BubbleSort()
+        {
+            int Min = 0;
+            int Max = 900;
+            Random aleatorio = new Random();
+
+            Console.Write("Introduce tamaño del arreglo");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[number]; //{ 2, 4, 7, 3, 11, 8 };
+
+            int upper = arr.Length - 1;
+            for(int i =0; i < number; i++)
+            {
+                arr[i] = aleatorio.Next(Min,Max);
+            }
+            int temp = 0;
+            for (int outer = upper; outer >= 1; outer--)
+            {
+
+                for (int inner = 0; inner <= outer - 1; inner++)
+                {
+                    if (arr[inner] > arr[inner + 1])
+                    {
+                        temp = arr[inner];
+                        arr[inner] = arr[inner + 1];
+                        arr[inner + 1] = temp;
+
+                    }
+                }
+            }
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            int x = 0, y = 0;
-            Console.WriteLine("Ingrese un numero");
-            x = int.Parse(Console.ReadLine());
-            for(int a =1; a <= x; a++)
-            {
-                if (x % a == 0)
-                    y++;
-                
-            }
-            if (y > 2)
-            {
-                Console.WriteLine("Falso ");
-            }else
-            {
-                Console.WriteLine("verdadero ");
-
-            }
-            Console.ReadKey();
+            ArrayBubble L = new ArrayBubble();
+            L.BubbleSort();
+            Console.ReadLine();
         }
     }
 }
